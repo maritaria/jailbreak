c7a803e52625cc778eaac471ed3d62713c19cf63
mooshbeef <bramkamies@gmail.com>
2015-07-09 22:07:35 +0200

    Missed a file during the previous commit
    Added new setting to gamemode to control the duration of the lastkill
    state

--- lua/jailbreak/sh_jailbreak_settings.lua
+++ lua/jailbreak/sh_jailbreak_settings.lua
@@ -6,6 +6,7 @@ function jailbreak:initSettings()
 	
 	self._wardenSetting = newInstance("Setting", manager, "Jailbreak.Warden", NULL);
 	self._minimumPlayerCountSetting = newInstance("Setting", manager, "Jailbreak.MinimumPlayerCount", 3);
+	self._lastKillDurationSetting = newInstance("Setting", manager, "Jailbreak.LastKillDuration", 10);
 	
 	self:getTeamBalancer():initSettings(manager);
 	self:getStateMachine():initSettings(manager);
@@ -17,4 +18,8 @@ end
 
 function jailbreak:getMinimumPlayerCountSetting()
 	return self._minimumPlayerCountSetting;
+end
+
+function jailbreak:getLastKillDurationSetting()
+	return self._lastKillDurationSetting;
 end
