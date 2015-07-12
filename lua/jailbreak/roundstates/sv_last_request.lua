bf39ca856a130345a07b51235814d76ba426f5f0
mooshbeef <bramkamies@gmail.com>
2015-07-12 12:07:16 +0200

    Linked last request state
    The last request state now switches to the last kill state when needed

--- lua/jailbreak/roundstates/sv_last_request.lua
+++ lua/jailbreak/roundstates/sv_last_request.lua
@@ -3,3 +3,17 @@ local lastRequest = newClass("Jailbreak.LastRequestState", "Jailbreak.RoundState
 function lastRequest:enter()
 	getDefinition("Jailbreak.RoundState").enter(self);
 end
+
+function lastRequest:tick()
+	getDefinition("Jailbreak.RoundState").tick(self);
+	
+	local prisonerTeam = self:getGamemode():getPrisonerTeam();
+	local guardTeam = self:getGamemode():getGuardTeam();
+	
+	local prisonerCount = self:getLiveCount(prisonerTeam);
+	local guardCount = self:getLiveCount(guardTeam);
+	
+	if (prisonerCount == 0) or (guardCount == 0) then
+		self:changeTo("LastKill");
+	end
+end
